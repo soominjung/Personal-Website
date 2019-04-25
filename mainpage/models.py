ae645fc75acc3ff803e278ef47ded7ab08e0fda2
soominjung <saneu_@naver.com>
2019-04-25 16:59:44 +0900

    fix error with db

--- mainpage/models.py
+++ mainpage/models.py
@@ -2,10 +2,3 @@ from django.db import models
 
 # Create your models here.
 
-
-class Testmodel(models.Model):
-    text = models.TextField()
-    author = models.CharField(max_length=200)
-
-    def approve(self):
-        self.save()
