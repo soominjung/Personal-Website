cb46f9417eb6696d2b237a33861754e4b2dcd66d
soominjung <saneu_@naver.com>
2019-04-25 15:52:12 +0900

    create a model

--- testapp/models.py
+++ testapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Testmodel(models.Model):
+    text = models.TextField()
+    author = models.CharField(max_length=200)
+
+    def approve(self):
+        self.save()
