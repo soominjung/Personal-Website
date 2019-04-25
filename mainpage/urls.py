ba5b2028d98458f92c180a21bf8ac42a07ff00a9
soominjung <saneu_@naver.com>
2019-04-25 18:37:40 +0900

    modify mainpage/urls

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from mainpage import views
 
 urlpatterns = [
-    path('mainpage/', views.post_list, name='post_list'),
+    path('', views.post_list, name='post_list'),
 ]
